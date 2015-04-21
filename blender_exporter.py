7a2c7defebb97c47bcfca355d6b14ccd72524a18
Angus Hollands <goosey15@gmail.com>
2015-04-21 19:36:54 +0100

    Update blender_exporter.py
    Add operator, support for vectors and eulers

--- blender_exporter.py
+++ blender_exporter.py
@@ -1,8 +1,11 @@
+import bpy
 from inspect import getmembers
+from json import dumps
+from mathutils import Vector, Euler
 
 
 def is_primitive(value):
-    return isinstance(value, (int, bool, str, float))
+    return isinstance(value, (int, bool, str, float, tuple))
 
 
 def get_primitive_attributes_as_dict(obj):
@@ -11,6 +14,9 @@ def get_primitive_attributes_as_dict(obj):
     for name, value in getmembers(obj):
         if name.startswith("_"):
             continue
+        
+        if isinstance(value, (Vector, Euler)):
+            value = tuple(value)
         
         if not is_primitive(value):
             continue
@@ -22,7 +28,8 @@ def get_primitive_attributes_as_dict(obj):
 
 def sca_to_dict(obj):
     as_dict = get_primitive_attributes_as_dict(obj)        
-    
+    if obj.type == "MOTION":
+        print(as_dict)
     if hasattr(obj, "controllers"):
         as_dict['controllers'] = [c.name for c in obj.controllers]
     
@@ -55,3 +62,36 @@ def dump_logic_bricks(obj):
 
     return dict(sensors=sensor_data, controllers=controller_data, actuators=actuator_data)        
 
+
+class LOGIC_OT_ExportLogicBricks(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "object.export_logic_bricks"
+    bl_label = "Export Logic Bricks To Text File"""
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        obj = context.object
+        converted = dump_logic_bricks(obj)
+        
+        try:
+            text = bpy.data.texts[obj.name]
+        except KeyError:
+            text = bpy.data.texts.new(obj.name)
+        
+        text.from_string(dumps(converted))
+        return {'FINISHED'}
+
+
+def register():
+    bpy.utils.register_class(LOGIC_OT_ExportLogicBricks)
+
+
+def unregister():
+    bpy.utils.unregister_class(LOGIC_OT_ExportLogicBricks)
+
+
+if __name__ == "__main__":
+    register()
